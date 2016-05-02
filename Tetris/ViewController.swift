6af58ffe0e21e4b32140539a2f95ebd85d95b5f9
student3 <student3@jh-mbair-158-02.d214.org>
2016-05-02 14:41:41 -0500

    NO

--- Tetris/ViewController.swift
+++ Tetris/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
   var TylerHasNoFriends = String()
+    var hi = string()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
