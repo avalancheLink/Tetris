b0b60cb06b46fa4fa65c8fc2201a834828b578c5
student3 <student3@jh-mbair-158-04.d214.org>
2016-05-03 14:31:47 -0500

    ROW 1

---
+++ Tetris/GameViewController.swift
@@ -0,0 +1,45 @@
+//
+//  GameViewController.swift
+//  Tetris
+//
+//  Created by student3 on 5/3/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var Label2: UILabel!
+    @IBOutlet weak var Label3: UILabel!
+    @IBOutlet weak var Label4: UILabel!
+    @IBOutlet weak var Label5: UILabel!
+    @IBOutlet weak var Label6: UILabel!
+    @IBOutlet weak var Label7: UILabel!
+    @IBOutlet weak var Label8: UILabel!
+    @IBOutlet weak var Label9: UILabel!
+    @IBOutlet weak var Label10: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
