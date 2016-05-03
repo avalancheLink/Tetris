86e464c5007cf55a882391f8f76b970be8c3cc35
student3 <student3@jh-mbair-158-13.d214.org>
2016-05-03 14:09:08 -0500

    cleaned

--- Tetris/ViewController.swift
+++ Tetris/ViewController.swift
@@ -1,7 +1,7 @@
-//lol2
+//
 //  ViewController.swift
 //  Tetris
-//1
+//
 //  Created by student3 on 5/2/16.
 //  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
 //
@@ -9,18 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-  var TylerHasNoFriends = String()
-    var hi = String()
-    var patrickNumber2IsSittingAcrossFromMe = String()
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
