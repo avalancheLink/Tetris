613997ab43ec7ce3ce5b9b077657e73c1b066498
student3 <student3@jh-mbair-158-13.d214.org>
2016-05-06 14:47:17 -0500

    ho hi

--- Tetris/ViewController.swift
+++ Tetris/ViewController.swift
@@ -1,14 +1,14 @@
-//lol2 Braum IS BEST
+//
 //  ViewController.swift
 //  Tetris
-//winston is not op
+//
 //  Created by student3 on 5/2/16.
 //  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
 //
 
 import UIKit
 import AVFoundation
-var lol = String()
+
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
