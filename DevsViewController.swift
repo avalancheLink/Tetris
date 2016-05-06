b066e8786eb819ba82cf9ba45b154bf4e8be7198
student3 <student3@jh-mbair-158-07.d214.org>
2016-05-06 14:30:21 -0500

    added madly pages

---
+++ DevsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DevsViewController.swift
+//  Tetris
+//
+//  Created by student3 on 5/6/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class DevsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onTapGoBackDev(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    
+}
