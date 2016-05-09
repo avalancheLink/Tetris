8317896e1d854211c93df79959604a4c2a59cf78
student3 <student3@jh-mbair-158-02.d214.org>
2016-05-09 14:47:46 -0500

    ok BACKGROUND IS GOOD

--- Tetris/ViewController.swift
+++ Tetris/ViewController.swift
@@ -1,14 +1,14 @@
-//lol2 Braum IS BEST
+
 //  ViewController.swift
 //  Tetris
-//winston is not op
+//  Patrick wants it and he got it
 //  Created by student3 on 5/2/16.
 //  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
 //
 
 import UIKit
 import AVFoundation
-var lol = String()
+
 class ViewController: UIViewController {
     
     @IBAction func whenTappedGoToTetrisGame(sender: UIButton) {
