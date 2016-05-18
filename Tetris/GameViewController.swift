96f4bf6294f7b9e2b3b738b707115511b3361953
student3 <student3@jh-mbair-158-07.d214.org>
2016-05-18 14:27:53 -0500

    hs

--- Tetris/GameViewController.swift
+++ Tetris/GameViewController.swift
@@ -439,10 +439,10 @@ class GameViewController: UIViewController {
 }
     @IBAction func goLeftWhenTapped(sender: UIButton) {
         if gameRunning == true {
-            if block1 % 10 != 0 {
-                if block2 % 10 != 0 {
-                    if block3 % 10 != 0 {
-                        if block4 % 10 != 0 {
+            if block1 % 10 != 1 {
+                if block2 % 10 != 1 {
+                    if block3 % 10 != 1 {
+                        if block4 % 10 != 1 {
                             labelArray[block1].backgroundColor = UIColor.blackColor()
                             labelArray[block2].backgroundColor = UIColor.blackColor()
                             labelArray[block3].backgroundColor = UIColor.blackColor()
@@ -464,10 +464,32 @@ class GameViewController: UIViewController {
     
     
     @IBAction func goRightWhenTapped(sender: UIButton) {
+        if gameRunning == true {
+            if block1 % 10 != 0 {
+                if block2 % 10 != 0 {
+                    if block3 % 10 != 0 {
+                        if block4 % 10 != 0 {
+                labelArray[block1].backgroundColor = UIColor.blackColor()
+                labelArray[block2].backgroundColor = UIColor.blackColor()
+                labelArray[block3].backgroundColor = UIColor.blackColor()
+                labelArray[block4].backgroundColor = UIColor.blackColor()
+                block1 = block1+1
+                block2 = block2+1
+                block3 = block3+1
+                block4 = block4+1
+                labelArray[block1].backgroundColor = color
+                labelArray[block2].backgroundColor = color
+                labelArray[block3].backgroundColor = color
+                labelArray[block4].backgroundColor = color
+                    }
+                }
+            }
+        }
     }
+}
     @IBAction func onTapRotateRight(sender: UIButton) {
     }
 
     @IBAction func onTapRotateLeft(sender: UIButton) {
     }
-}}
+}
